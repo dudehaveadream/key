eca0892ef17b1b32a9eee04852652f72d3694580
dudehaveadream <heyandydude@gmail.com>
2015-08-29 09:05:49 +0300

    sidebar update

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
-  var $btn = $('.search-btn'),
-      $inputWrap = $('.search-input_wrap'),
-      $body = $('body');
 
-  $btn.on('click', function(e){
-    e.preventDefault();
-    $(this).parent().toggleClass('show-input');
-  });
+    // Search func
+    var $btn = $('.search-btn'),
+        $inputWrap = $('.search-input_wrap'),
+        $body = $('body');
+    $btn.on('click', function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass('show-input');
+    });
+    $body.on('click', function(){
+        $inputWrap.removeClass('show-input');
+    });
+    $inputWrap.on('click', function(){
+        return false;
+    });
 
-  $body.on('click', function(){
-    $inputWrap.removeClass('show-input');
-  });
-
-  $inputWrap.on('click', function(){
-    return false;
-  })
+    // Sidebar func
+    $('.sidebar-subnav').hide();
+    $('.sidebar-nav > li > a').click(function(e){
+        $(this).parent().find('.sidebar-subnav').slideToggle();
+        $(this).parent().addClass('open');
+        $(this).parent().siblings().removeClass('open')
+        $(this).parent().siblings().find('.sidebar-subnav').slideUp();
+        e.preventDefault();
+    });
   
 });
